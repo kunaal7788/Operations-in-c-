c5a69a79f585cb3e4a09b827f393080e428da15d
Kunal Digole <kunal.digole94@gmail.com>
2022-10-12 22:25:10 +0530

    First commit

---
+++ Operators in c++.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter Number1"<<endl;
+    cin>> x;
+    cout <<"Enter Number2"<<endl;
+    cin>>y;
+
+    cout<< "x+y will be "<< x+y<<endl;
+    cout<< "x-y will be "<< x-y<<endl;
+    cout<< "x*y will be "<< x*y<<endl;
+    cout<< "x/y will be "<< x/y<<endl;
+    return 0;
+
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int age ;
+      cout<<"Enter age"<<endl ;
+      cin >> age;
+    if (age>= 21 ) {
+        cout<<"You Can Play" ;
+      }
+    else{
+        cout<<"You can't Play"<<endl;
+      }
+    return 0;
+
+}
